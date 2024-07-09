2af9e9e2c9f58d2bfb5aa30cf3ae6981356ed3f7
KaiLiu12 <74182067+onetrickollie@users.noreply.github.com>
2024-07-09 11:10:29 -0700

    initial commit

--- SafeWalkVolunteer/Volunteer.swift
+++ SafeWalkVolunteer/Volunteer.swift
@@ -3,6 +3,8 @@
 //  SafeWalkVolunteer
 //  Lab exercise Created by Paul Inventado on 2/9/22.
 //
+//  Author: Kaixiang Liu, 884780172
+//  7/9/2024
 
 import Foundation
 
@@ -13,9 +15,36 @@ protocol Volunteer: ObservableObject {
 }
 
 // TODO: Create a class called SafeWalkVolunteer that implementes the Volunteer protocol.
+class SafeWalkVolunteer: Volunteer{
+    var name: String
+    var age: Int
 
 // TODO: Create an Int maxHours computed property. It is a get-only property that returns 1 if the volunteer's age is less than 18 and returns 3 if the volunteer's age is 18 and over.
+    var maxHours: Int{
+        if(age<18){
+            return 1
+        }
+        else{
+            return 3
+        }
+    }
 
 // TODO: Create an initializer without parameters that sets name to an empty string and age to 0.
-
+    init() {
+        self.age = 0
+        self.name = ""
+    }
+    
 // TODO: Create another initializer that accepts a a String parameter called name and an Int parameter called age. Assign the parameter values to the corresponding properties of the class.
+
+    init(name:String, age: Int){
+        self.age = age
+        self.name = name
+    }
+    
+    
+    
+}
+
+
+
